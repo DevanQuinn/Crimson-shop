45cea7aa54b958d6d67cd886456dfced78118367
-global <62554326+DevanQuinn@users.noreply.github.com>
2021-04-16 02:01:20 -0400

    first commit

--- server.ts
+++ server.ts
@@ -2,6 +2,6 @@ const isProduction: boolean = process.env.NODE_ENV === 'production';
 
 const server: string = !isProduction
 	? 'http://localhost:3000'
-	: 'https://crimsonathletics.shop';
+	: 'https://crimson-shop.herokuapp.com/';
 export default server;
 export { isProduction };
